f04767ef76fdd651973def82fd8f3c68df7fbb9a
anazi6j <36531957+anazi6j@users.noreply.github.com>
2018-12-12 09:35:29 +0900

    Update AnimationCopy_Test.cs

--- AnimationCopy_Test.cs
+++ AnimationCopy_Test.cs
@@ -1,34 +1,41 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+namespace robot { 
 public class AnimationCopy_Test : MonoBehaviour
 {
     public EnemyParamTable epm;
-    
-    public List<CloseAttacks> C= new List<CloseAttacks>();
+
+    public List<CloseAttacks> C;
     // Start is called before the first frame update
     void Start()
     {
+
+
+            C = new List<CloseAttacks>();
+
+            for (int i = 0; i < epm.closeAttacks.Count; i++)
+            {
+                C.Add(new CloseAttacks(epm.closeAttacks[i].attackanim, epm.closeAttacks[i].attackpower, epm.closeAttacks[i].attackstrength,
+              epm.closeAttacks[i].intervaltime, epm.closeAttacks[i].canbeParried, epm.closeAttacks[0].colliders));
+            }
+
+
+
+
+
+        }
         
-       
-            List<CloseAttacks> closes = new List<CloseAttacks>();
-
-            closes[0].attackanim = epm.closeAttacks[0].attackanim;
-            closes[0].attackpower = epm.closeAttacks[0].attackpower;
-            closes[0].attackstrength = epm.closeAttacks[0].attackstrength;
-            closes[0].intervaltime = epm.closeAttacks[0].intervaltime;
-            C.Add(closes[0]);
-            Debug.Log("テストコピー開始");
 
         
 
 
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
         
     }
+}
 }
